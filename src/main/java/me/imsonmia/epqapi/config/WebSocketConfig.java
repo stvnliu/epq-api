b70677cfbbc530d40b5baa94df20c90bf5ffbfc6
Zhongheng Liu <z.liu@outlook.com.gr>
2023-12-21 19:07:19 +0200

    clean working tree

--- src/main/java/me/imsonmia/epqapi/config/WebSocketConfig.java
+++ src/main/java/me/imsonmia/epqapi/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/sub");
-        registry.setApplicationDestinationPrefixes("/app");   
+        registry.setApplicationDestinationPrefixes("/app");
     }
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
